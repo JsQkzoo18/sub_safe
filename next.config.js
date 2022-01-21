2d46e221b4c1a6a29df972fad93837591f6fde35
John Silva <jhonmvl18@gmail.com>
2022-01-20 20:03:38 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  distDir: "build",
+  reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
